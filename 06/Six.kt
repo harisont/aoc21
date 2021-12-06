7413bf3a1f47a00257468f21103e4429126e1bcc
= <=>
2021-12-07 00:11:16 +0100

    an eminently non-functional solution to advent 7

---
+++ 06/Six.kt
@@ -0,0 +1,29 @@
+import java.io.File
+
+typealias Lanternfish = Int
+
+fun main() {
+    val fish = File("input.txt")
+                .readText()
+                .replace("\n", "")
+                .split(",")
+                .map { it.toInt() }
+                .toMutableList()
+        
+    for (i in 1..80) {
+        var nNews = 0
+        for (f in 0..fish.size - 1) {
+            if (fish[f] == 0) {
+                fish[f] = 6
+                nNews += 1
+            }
+            else
+                fish[f] -= 1
+        }
+        for (f in 1..nNews) {
+            fish.add(8)
+        }
+    }
+
+    println("Part 1: ${fish.size}")
+}

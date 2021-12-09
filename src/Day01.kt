43cb42ab5250642a02d54af7ee8ca72f0bbe9420
= <=>
2021-12-10 00:43:46 +0100

    better solution for advent 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -2,28 +2,17 @@ package bho.harisont.aoc.day01
 
 import java.io.File
 
-fun readReport(path: String): List<Int> {
-    return File(path).readLines().map{ it.toInt() }
-}
+fun readReport(path: String): List<Int> =
+    File(path).readLines().map{ it.toInt() }
 
-
-fun nIncreases(ints: List<Int>): Int {
+fun nIncreases(ns: List<Int>, win: Int=1): Int {
     var increases = 0
-    for (i in 0..ints.size - 2) {
-        if (ints[i + 1] > ints[i])
-            increases++
-    }
-    return increases
-}
-
-fun n3Increases(ints: List<Int>): Int {
-    var increases = 0
-    for (i in 0..ints.size - 4) {
-        if (ints.slice(i + 1..i + 3).sum() > ints.slice(i..i + 2).sum())
+    for (i in 0..ns.size - (win + 1)) {
+        if (ns.slice(i + 1..i + win).sum() > ns.slice(i..i + (win - 1)).sum())
             increases++
     }
     return increases
 }
 
 fun part1(path: String): Int = nIncreases(readReport(path))
-fun part2(path: String): Int = n3Increases(readReport(path))
+fun part2(path: String): Int = nIncreases(readReport(path), 3)
